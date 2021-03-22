8f6d184e81d439d99e8d65f14ace7a3a362a6c74
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-03-22 18:22:04 +0000

    Add time update logic and display time in panel

--- Assets/Scripts/GameTime.cs
+++ Assets/Scripts/GameTime.cs
@@ -11,11 +11,73 @@ public static class GameTime {
     static private int year;
     static private Month month;
     static private int day;
+    static private float dayFraction;
+
+    static private int gameSpeed = 1;
 
     static public void Set(int y, Month m, int d) {
         year = y;
         month = m;
         day = d;
+        dayFraction = 0f;
+    }
+
+    static public void SetSpeed(int speed) {
+        gameSpeed = speed;
+    }
+
+    static public void UpdateTime() {
+        dayFraction += gameSpeed * Time.unscaledDeltaTime;
+
+        if (dayFraction >= 1) {
+            day++;
+            dayFraction--;
+        }
+
+        switch (month) {
+            case Month.Feb:
+                // Check for leap year
+                if (year % 400 == 0 | (year % 4 == 0 && year % 100 != 0)) {
+                    // leap year
+                    if (day > 29) {
+                        month++;
+                        day = 1;
+                    }
+                }
+                else {
+                    // not leap year
+                    if (day > 28) {
+                        month++;
+                        day = 1;
+                    }
+                }
+                break;
+
+            case Month.Apr:
+            case Month.Jun:
+            case Month.Sep:
+            case Month.Nov:
+                if (day > 30) {
+                    month++;
+                    day = 1;
+                }
+                break;
+
+            case Month.Dec:
+                if (day > 31) {
+                    year++;
+                    month = Month.Jan;
+                    day = 1;
+                }
+                break;
+
+            default:
+                if (day > 31) {
+                    month++;
+                    day = 1;
+                }
+                break;
+        }
     }
 
     static public string GetShortForm() {
