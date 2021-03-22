be4d2f8c734671fd6bbfea73b772c47da7edce9d
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-03-22 21:04:50 +0000

    Add UI element for date

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TimeControl : MonoBehaviour
 {
-
+    public UnityEngine.UI.Text date;
 
     // Start is called before the first frame update
     void Start()
     {
         GameTime.Set(1800, (Month)0, 1);
+        GameTime.SetSpeed(1);
         // Debug.Log(GameTime.Get());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameTime.UpdateTime();
+        date.text = GameTime.GetShortForm();
     }
 }
