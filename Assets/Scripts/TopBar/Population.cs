f507231f29b5c6e5874af7815bdd854c6d417d88
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-03-31 12:02:43 +0100

    Add total population to top bar

---
+++ Assets/Scripts/TopBar/Population.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TopBar {
+
+    public class Population : MonoBehaviour {
+
+        public Text population;
+        public HexGrid grid;
+
+        // Start is called before the first frame update
+        void Start() {
+        }
+
+
+        // Update is called once per frame
+        void Update() {
+            population.text = grid.GetTotalPopulation().ToString();
+        }
+    }
+}
+
