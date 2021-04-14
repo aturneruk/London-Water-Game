079f4d7dea11b8e8c594b20353cf54192539b657
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-14 20:51:55 +0100

    Tidy up TopBar.Population

--- Assets/Scripts/TopBar/Population.cs
+++ Assets/Scripts/TopBar/Population.cs
@@ -10,12 +10,6 @@ namespace TopBar {
         public Text population;
         public HexGrid grid;
 
-        // Start is called before the first frame update
-        void Start() {
-        }
-
-
-        // Update is called once per frame
         void Update() {
             population.text = "Population: " + grid.GetTotalPopulation().ToString();
         }
