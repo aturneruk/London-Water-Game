5bfc7b26d2db35e93ec9791d68a64d039d680a01
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-15 16:09:21 +0100

    Update Water.cs
    - Add groundwater bool
    - Add data validation to Volume.Set when groundwater == true
    - Add 3 parameter constructor to allow groundwater to be set, including checks
    - Add Water.Product method to get product of Volume and Quality

--- Assets/Scripts/Water/Water.cs
+++ Assets/Scripts/Water/Water.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Water {
-    public struct Water
-    {
+    public struct Water {
         private float volume;
         private float quality;
+        private bool groundwater;
 
         public float Volume {
             get {
@@ -14,6 +14,12 @@ namespace Water {
             }
             set {
                 volume = value;
+
+                if (groundwater == true) {
+                    if (volume > 5000000000) {
+                        throw new System.ArgumentException("Maximum volume is 5e9 L");
+                    }
+                }
             }
         }
 
@@ -40,9 +46,37 @@ namespace Water {
                 throw new System.ArgumentException("The quality must be between 0 and 1 inclusive, value is " + quality);
             }
 
+            groundwater = false;
             this.volume = volume;
         }
 
+        public Water(float volume, float quality, bool groundwater) {
+
+            if (quality >= 0 && quality <= 1) {
+                this.quality = quality;
+            }
+            else {
+                throw new System.ArgumentException("The quality must be between 0 and 1 inclusive, value is " + quality);
+            }
+
+            if (groundwater == true) {
+                if (volume > 5000000000) {
+                    throw new System.ArgumentException("Maximum volume is 5e9 L");
+                }
+                this.groundwater = true;
+            }
+            else {
+                this.groundwater = false;
+            }
+
+            this.volume = volume;
+        }
+
+        public float Product {
+            get {
+                return Quality * Volume;
+            }
+        }
     }
 }
 
