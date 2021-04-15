1a23a461894435ef9c1ca8ff0856f91429f4e7b8
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-15 16:12:19 +0100

    Refactor Water.Groundwater
    - Use Water.Water struct instead of separate Volume and Quality
    - Rewrite Groundwater.Level property to reflect changes caused by using Water.Water struct
    - New Groundwater.Infiltrate method

--- Assets/Scripts/Water/Groundwater.cs
+++ Assets/Scripts/Water/Groundwater.cs
@@ -7,56 +7,25 @@ namespace Water {
     public class Groundwater {
 
         private readonly Manager manager;
-        private float level;
-        private float quality;
-        private float volume;
+        public Water Storage;
         private float maxAbstraction = 600000;
 
-        public float Level { 
+        public float Level {
             get {
-                return level;
-            }
-            private set {
-                if (value >= 0 && value <= 1) {
-                    level = value;
+                float level = 0.0002f * Storage.Volume / (float)manager.hexCell.area;
+
+                if (level >= 0 && level <= 1) {
+                    return level;
                 }
                 else {
-                    throw new System.ArgumentException("The groundwater level must be between 0 and 1 inclusive, value is " + value + " in cell " + manager.hexCell.index);
+                    throw new System.ArgumentException("The groundwater level must be between 0 and 1 inclusive, value is " + level + " in cell " + manager.hexCell.index);
                 }
-            }
-        }
-
-        public float Quality {
-            get {
-                return quality;
-            }
-            private set {
-                if (value >= 0 && value <= 1) {
-                    quality = value;
-                }
-                else {
-                    throw new System.ArgumentException("The quality must be between 0 and 1 inclusive, value is " + value + " in cell " + manager.hexCell.index);
-                }
-            }
-        }
-
-        public float Volume {
-            get {
-                return volume;
-            }
-            private set {
-                if (value > 5000000000) {
-                    throw new System.ArgumentException("Maximum volume is 5e9 L");
-                }
-                volume = value;
-                Level = 0.0002f * volume / (float)manager.hexCell.area; 
             }
         }
 
         public Groundwater(Manager manager) {
             this.manager = manager;
-            Volume = 5000000000; // L
-            Quality = 1;
+            Storage = new Water(5000000000, 1, true);
         }
 
         public float GetMaxAbstraction {
@@ -67,15 +36,21 @@ namespace Water {
 
         public Water Abstract(float abstraction) {
             if (abstraction <= GetMaxAbstraction) {
-                Volume -= abstraction;
-                return new Water(abstraction, Quality);
+                Storage.Volume -= abstraction;
+                return new Water(abstraction, Storage.Quality);
                 }
             else {
                 abstraction = GetMaxAbstraction;
-                Volume -= abstraction;
-                return new Water(abstraction, Quality);
+                Storage.Volume -= abstraction;
+                return new Water(abstraction, Storage.Quality);
             }
         }
+
+        public void Infiltrate(Water input) {
+            Storage.Quality = (Storage.Product + input.Product) / (Storage.Volume + input.Volume);
+            Storage.Volume += input.Volume;
+        }
+
     }
 }
 
