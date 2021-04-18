ca5313ac8b6defebb614c613736bb34807b50234
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-18 16:45:36 +0100

    New UI Namespace

--- Assets/Scripts/UI/TimeControl.cs
+++ Assets/Scripts/UI/TimeControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace TopBar {
+namespace UI {
     public class TimeControl : MonoBehaviour {
         public Text date;
         public Button speedControl;
