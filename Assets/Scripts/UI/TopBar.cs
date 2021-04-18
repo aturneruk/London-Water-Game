a8e701b3a5a7cc066ec3edd6a1273d5163f862ca
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-18 17:51:07 +0100

    Delete redundant lines

--- Assets/Scripts/UI/TopBar.cs
+++ Assets/Scripts/UI/TopBar.cs
@@ -13,19 +13,15 @@ namespace UI {
 
         public HexGrid grid;
 
-        // Start is called before the first frame update
         void Start() {
             GameTime.Set(1800, Month.Jan, 1, Weekday.Mon);
             GameTime.SetSpeed(Speed.X1);
             speedControl.GetComponentInChildren<Text>().text = Speed.X1.ToString();
-            // Debug.Log(GameTime.Get());
         }
 
-        // Update is called once per frame
         void Update() {
             GameTime.UpdateTime();
             date.text = GameTime.GetLongForm();
-
             population.text = "Population: " + grid.GetTotalPopulation().ToString();
         }
 
@@ -43,8 +39,6 @@ namespace UI {
             }
 
             speedControl.GetComponentInChildren<Text>().text = speed.ToString();
-
-            Sprite image = speedControl.GetComponent<Image>().sprite;
         }
     }
 }
