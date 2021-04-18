a6678057734336dbe3e146512bb55e597c30ee07
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-18 23:30:16 +0100

    Update DataOverlay.cs
    - Move code blocks
    - Add separate PopulationColor method
    - Remove InverseLerp to allow scaled populations over 1
    - Refresh colors every day

--- Assets/Scripts/UI/DataOverlay.cs
+++ Assets/Scripts/UI/DataOverlay.cs
@@ -9,32 +9,31 @@ namespace UI {
         None, Population, GroundwaterQuality
     }
 
+    public class DataOverlay : MonoBehaviour {
+
+        public HexGrid hexGrid;
+
+        public void UpdateOverlay() {
+            int index = gameObject.GetComponent<Dropdown>().value;
+            Overlay overlay = (Overlay)index;
+            hexGrid.SetDataOverlay(overlay);
+        }
+
+        private void OnEnable() {
+            GameTime.NewDay += UpdateOverlay;
+        }
+
+        private void OnDisable() {
+            GameTime.NewDay -= UpdateOverlay;
+        }
+    }
+
     public static class OverlayExtensions {
 
         public static Color? CellColor(this Overlay overlay, HexCell cell) {
 
             if (overlay == Overlay.Population) {
-
-                float population = cell.Population.Size;
-
-                if (population > 100000) {
-                    throw new System.ArgumentOutOfRangeException("Population is greater than 100000");
-                }
-                else {
-                    // scale to set x=1 to a convenient point for the function below
-                    float val = Mathf.InverseLerp(0, 30000, population);
-
-                    // This function has a horiztonal asymtote at y = 1.0 :-)
-                    // at x = 1, y = 0.74
-                    if (val > 0) {
-                        val = 0.1f + 0.9f * Mathf.Sqrt(val / (val + 1));                        
-                    }
-                    else {
-                        val = 0;
-                    }
-
-                    return new Color(1f, 1 - val, 1f);
-                }
+                PopulationColor(cell);                
             }
             else if (overlay == Overlay.GroundwaterQuality) {
                 return null;
@@ -43,17 +42,36 @@ namespace UI {
                 return null;
             }
         }
-    }
 
-    public class DataOverlay : MonoBehaviour {
+        public static Color PopulationColor(HexCell cell) {
+            float population = cell.Population.Size;
 
-        public HexGrid hexGrid;
+            if (population > 100000) {
+                throw new System.ArgumentOutOfRangeException("Population is greater than 100000");
+            }
+            else {
+                // scale to set x=1 to a convenient point for the function below
+                float val = population / 30000f;
+                if (val > 1) {
+                    Debug.Log(val);
+                }
 
-        public void ChangeOverlay() {
-            int index = gameObject.GetComponent<Dropdown>().value;
-            Overlay overlay = (Overlay)index;
-            hexGrid.SetDataOverlay(overlay);
+                // This function has a horiztonal asymtote at y = 1.0 :-)
+                // at x = 1, y = 0.74
+                if (val > 0) {
+                    val = 0.1f + 0.9f * Mathf.Sqrt(val / (val + 1));
+                }
+                else {
+                    val = 0;
+                }
+
+                return new Color(1f, 1 - val, 1f);
+            }
         }
+
+
     }
+
+
 }
 
