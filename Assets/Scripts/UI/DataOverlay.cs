f9c029547305dac689a1b2a6a0adb74eca325411
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-18 16:59:56 +0100

    Basic DataOverlay class and component

---
+++ Assets/Scripts/UI/DataOverlay.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+
+    public class DataOverlay : MonoBehaviour {
+
+        public HexGrid hexGrid;
+
+
+    }
+}
+
