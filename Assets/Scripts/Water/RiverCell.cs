2f845cb7f1e0fbb18c8ba5647b439a0854a501f6
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-20 16:55:07 +0100

    Basic RiverCell class

---
+++ Assets/Scripts/Water/RiverCell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Water {
+    public class RiverCell : MonoBehaviour {
+
+        public int index;
+        public Water Water;
+
+
+        private void Awake() {
+            
+        }
+
+
+    }
+}
+
+
