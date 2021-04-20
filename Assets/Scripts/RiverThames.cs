ab6a6a82975f3407ebeff43ef08d3dc305a68ef9
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-20 16:54:56 +0100

    RiverThames
    - Use cell indices not offset coords
    - Use as a component of Hex Grid rather than separate River Thames scene object

--- Assets/Scripts/RiverThames.cs
+++ Assets/Scripts/RiverThames.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class RiverThames : MonoBehaviour {
 
-    public HexGrid hexGrid;
+    private HexGrid hexGrid;
 
     public static HexCell[] riverCells;
-    private static int[,] cellOffsetCoords;
 
     private int riverLength;
 
@@ -15,107 +14,24 @@ public class RiverThames : MonoBehaviour {
     public float[] quality;
     public float totalQuality;
 
-    private void Awake() {
+    private void Start() {
+        hexGrid = gameObject.GetComponent<HexGrid>();
         SetupRiverCells();
         Water.FlowNetwork.GenerateNetwork();
         AssignRiversideCells();
     }
 
-    private void Start() {
-        // StartCoroutine(ShowFlow());
-    }
+    //private void Start() {
+    //    // StartCoroutine(ShowFlow());
+    //}
 
     private void SetupRiverCells() {
-        cellOffsetCoords = new int[,] {
-            { 0, 4 },
-            { 0, 3 },
-            { 1, 2 },
-            { 1, 1 },
-            { 2, 1 },
-            { 3, 1 },
-            { 4, 2 },
-            { 4, 3 },
-            { 5, 3 },
-            { 6, 3 },
-            { 7, 3 },
-            { 8, 2 },
-            { 8, 1 },
-            { 9, 1 },
-            { 10, 2 },
-            { 10, 3 },
-            { 11, 4 },
-            { 10, 5 },
-            { 9, 5 },
-            { 9, 6 },
-            { 9, 7 },
-            { 10, 7 },
-            { 11, 8 },
-            { 10, 9 },
-            { 9, 9 },
-            { 9, 10 },
-            { 9, 11 },
-            { 10, 12 },
-            { 10, 13 },
-            { 11, 13 },
-            { 12, 12 },
-            { 12, 11 },
-            { 13, 11 },
-            { 14, 12 },
-            { 14, 13 },
-            { 15, 13 },
-            { 16, 12 },
-            { 15, 11 },
-            { 16, 10 },
-            { 17, 10 },
-            { 18, 10 },
-            { 18, 11 },
-            { 19, 12 },
-            { 20, 12 },
-            { 21, 12 },
-            { 22, 12 },
-            { 22, 13 },
-            { 23, 14 },
-            { 23, 15 },
-            { 24, 15 },
-            { 25, 15 },
-            { 26, 15 },
-            { 27, 15 },
-            { 28, 15 },
-            { 29, 15 },
-            { 30, 14 },
-            { 30, 13 },
-            { 31, 12 },
-            { 32, 12 },
-            { 32, 13 },
-            { 32, 14 },
-            { 32, 15 },
-            { 33, 15 },
-            { 34, 14 },
-            { 35, 14 },
-            { 36, 14 },
-            { 37, 14 },
-            { 37, 15 },
-            { 38, 15 },
-            { 39, 16 },
-            { 40, 16 },
-            { 40, 15 },
-            { 41, 15 },
-            { 42, 14 },
-            { 43, 14 },
-            { 43, 13 },
-            { 44, 12 },
-            { 45, 12 },
-            { 46, 12 },
-            { 46, 11 },
-            { 47, 10 },
-            { 47, 9 }
-        };
-
-        riverLength = cellOffsetCoords.GetLength(0);
+        
+        riverLength = cellIndices.Length;
         riverCells = new HexCell[riverLength];
 
         for (int i = 0; i < riverLength; i++) {
-            HexCell cell = hexGrid.GetCellFromOffset(cellOffsetCoords[i, 0], cellOffsetCoords[i, 1]);
+            HexCell cell = hexGrid.GetCellFromIndex(cellIndices[i]);
             riverCells[i] = cell;
             cell.isThames = true;
             cell.hasRiver = true;
@@ -149,14 +65,6 @@ public class RiverThames : MonoBehaviour {
         }
     }
 
-    //private IEnumerator ShowFlow() {
-    //    for (int i = 0; i < riverLength; i++) {
-    //        yield return new WaitForSeconds(0.3f);
-    //        riverCells[i].Color = Color.blue;
-    //    }
-    //}
-
-
     public void Discharge(float flow, float quality) {
 
     }
@@ -164,6 +72,91 @@ public class RiverThames : MonoBehaviour {
     public void Abstract(float flow, HexCell cell) {
 
     }
+
+    private int[] cellIndices = {
+        192,
+        144,
+        97,
+        49,
+        50,
+        51,
+        100,
+        148,
+        149,
+        150,
+        151,
+        104,
+        56,
+        57,
+        106,
+        154,
+        203,
+        250,
+        249,
+        297,
+        345,
+        346,
+        395,
+        442,
+        441,
+        489,
+        537,
+        586,
+        634,
+        635,
+        588,
+        540,
+        541,
+        590,
+        638,
+        639,
+        592,
+        543,
+        496,
+        497,
+        498,
+        546,
+        595,
+        596,
+        597,
+        598,
+        646,
+        695,
+        743,
+        744,
+        745,
+        746,
+        747,
+        748,
+        749,
+        702,
+        654,
+        607,
+        608,
+        656,
+        704,
+        752,
+        753,
+        706,
+        707,
+        708,
+        709,
+        757,
+        758,
+        807,
+        808,
+        760,
+        761,
+        714,
+        715,
+        667,
+        620,
+        621,
+        622,
+        574,
+        527,
+        479    
+    };
 }
 
 
