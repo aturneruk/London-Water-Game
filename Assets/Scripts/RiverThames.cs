0aef2354d56183eda5292ce3478ec1c9c3d25380
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-23 02:27:20 +0100

    Implement the river and make it flow

--- Assets/Scripts/RiverThames.cs
+++ Assets/Scripts/RiverThames.cs
@@ -2,192 +2,253 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RiverThames : MonoBehaviour {
+namespace Water {
+    public class RiverThames : MonoBehaviour {
 
-    private HexGrid hexGrid;
-    private List<HexCell> riverCells = new List<HexCell>();
-    public int RiverLength { get; private set; }
+        private HexGrid hexGrid;
+        private List<HexCell> hexCells = new List<HexCell>();
+        private List<RiverCell> riverCells = new List<RiverCell>();
+        public int RiverLength { get; private set; }
 
-    private void Start() {
-        hexGrid = gameObject.GetComponent<HexGrid>();
+        [SerializeField]
+        UnityEngine.UI.Slider inflowSlider;
 
-        for (int i = 0; i < cellIndices.Length; i++) {
+        private void Awake() {
 
-            int hexCellIndex = cellIndices[i];
-            HexCell cell = hexGrid.GetCellFromIndex(hexCellIndex);
-            cell.riverDistance = 0;
+            hexGrid = gameObject.GetComponent<HexGrid>();
 
-            Water.RiverCell riverCell = cell.gameObject.AddComponent<Water.RiverCell>();
-            riverCell.index = i;
-
-            riverCells.Add(cell);
+            CreateRiverCells();
+            GenerateNetwork();
         }
 
-        RiverLength = riverCells.Count;
+        private void OnEnable() {
+            GameTime.NewDay += DailyRefresh;
+        }
 
-        GenerateNetwork();
-    }
+        private void OnDisable() {
+            GameTime.NewDay -= DailyRefresh;
+        }
 
-    public void GenerateNetwork() {
+        private void CreateRiverCells() {
 
-        List<HexCell> cells = new List<HexCell>(riverCells);
-        List<HexCell> nextCells = new List<HexCell>();
+            for (int i = 0; i < cellIndices.Length; i++) {
 
-        System.Random rng = new System.Random();
+                int hexCellIndex = cellIndices[i];
+                HexCell hexCell = hexGrid.GetCellFromIndex(hexCellIndex);
+                hexCells.Add(hexCell);
+                hexCell.riverDistance = 0;
 
-        bool complete = false;
-        int level = 1;
-        int changed = 0;
+                RiverCell riverCell = hexCell.gameObject.AddComponent<RiverCell>();
+                riverCells.Add(riverCell);
+                riverCell.river = this;
+                riverCell.hexCell = hexCell;
+                riverCell.index = i;
+                if (i > 0) {
+                    riverCell.PreviousCell = riverCells[i - 1];
+                    riverCells[i - 1].NextCell = riverCell;
+                }
+            }
 
-        while (!complete) {
+            RiverLength = riverCells.Count;
+        }
 
-            foreach (HexCell cell in cells) {
+        public void GenerateNetwork() {
+
+            GenerateNetwork2();
+
+            // Set discharge cell
+            for (int i = 0; i < RiverLength; i++) {
 
                 for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
 
-                    HexCell neighbor = cell.GetNeighbor(d);
+                    HexCell neighbor = hexCells[i].GetNeighbor(d);
 
-                    if (neighbor && neighbor.riverDistance == null) {
-                        neighbor.riverDistance = level;
-                        changed++;
-                        nextCells.Add(neighbor);
+                    if (neighbor && neighbor.riverDistance == 1) {
+                        neighbor.dischargeCell = hexCells[i];
+                    }
+                }
+            }
 
-                        if (level > 1) {
-                            bool set = false;
+            // Set abstraction cell
+            for (int i = RiverLength - 1; i >= 0; i--) {
 
-                            while (!set) {
-                                int random = rng.Next(6);
-                                HexCell neighbor1 = neighbor.GetNeighbor((HexDirection)random);
-                                if (neighbor1 && neighbor1.riverDistance == level - 1) {
-                                    neighbor.dischargeCell = neighbor1;
-                                    set = true;
+                for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+
+                    HexCell neighbor = hexCells[i].GetNeighbor(d);
+
+                    if (neighbor && neighbor.riverDistance == 1) {
+                        neighbor.abstractionCell = hexCells[i];
+                    }
+                }
+            }
+        }
+
+        private void GenerateNetwork2() {
+
+            List<HexCell> currentCells = new List<HexCell>(hexCells);
+            List<HexCell> nextCells = new List<HexCell>();
+
+            System.Random rng = new System.Random();
+
+            int level = 1;
+            int changed = 0;
+
+            while (true) {
+
+                foreach (HexCell cell in currentCells) {
+
+                    for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+
+                        HexCell neighbor = cell.GetNeighbor(d);
+
+                        if (neighbor && neighbor.riverDistance == null) {
+                            neighbor.riverDistance = level;
+                            changed++;
+                            nextCells.Add(neighbor);
+
+                            if (level > 1) {
+                                bool set = false;
+
+                                while (!set) {
+                                    int random = rng.Next(6);
+                                    HexCell neighbor1 = neighbor.GetNeighbor((HexDirection)random);
+                                    if (neighbor1 && neighbor1.riverDistance == level - 1) {
+                                        neighbor.dischargeCell = neighbor1;
+                                        set = true;
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
 
-            if (changed == 0) {
-                complete = true;
-            }
-
-            level++;
-            changed = 0;
-            cells.Clear();
-            cells.AddRange(nextCells);
-            nextCells.Clear();
-        }
-
-        // Set discharge cell
-        for (int i = 0; i < RiverLength; i++) {
-            for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
-
-                HexCell neighbor = riverCells[i].GetNeighbor(d);
-
-                if (neighbor && neighbor.riverDistance == 1) {
-                    neighbor.dischargeCell = riverCells[i];                                        
+                if (changed == 0) {
+                    return;
                 }
+
+                level++;
+                changed = 0;
+                currentCells.Clear();
+                currentCells.AddRange(nextCells);
+                nextCells.Clear();
             }
         }
 
-        // Set abstraction cell
-        for (int i = RiverLength - 1; i >= 0; i--) {
-            for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
+        private float inflowValueScalar = 1;
 
-                HexCell neighbor = riverCells[i].GetNeighbor(d);
+        public Water GetInflow(RiverCell riverCell) {
 
-                if (neighbor && neighbor.riverDistance == 1) {
-                    neighbor.abstractionCell = riverCells[i];
-                }
+            switch (riverCell.index) {
+                case 0:
+                    return new Water(5200000000 * inflowValueScalar, 1);
+                default:
+                    return new Water(0, 1);
             }
-        }        
+        }
+
+        public void UpdateSlider() {
+            inflowValueScalar = inflowSlider.value;
+        }
+
+        bool toggle = false;
+
+        private void DailyRefresh() {
+
+            toggle ^= true;
+
+            foreach (RiverCell riverCell in riverCells) {
+                riverCell.UpdateFlow(toggle);
+            }
+
+        }
+
+
+        private int[] cellIndices = {
+            192,
+            144,
+            97,
+            49,
+            50,
+            51,
+            100,
+            148,
+            149,
+            150,
+            151,
+            104,
+            56,
+            57,
+            106,
+            154,
+            203,
+            250,
+            249,
+            297,
+            345,
+            346,
+            395,
+            442,
+            441,
+            489,
+            537,
+            586,
+            634,
+            635,
+            588,
+            540,
+            541,
+            590,
+            638,
+            639,
+            592,
+            543,
+            496,
+            497,
+            498,
+            546,
+            595,
+            596,
+            597,
+            598,
+            646,
+            695,
+            743,
+            744,
+            745,
+            746,
+            747,
+            748,
+            749,
+            702,
+            654,
+            607,
+            608,
+            656,
+            704,
+            752,
+            753,
+            706,
+            707,
+            708,
+            709,
+            757,
+            758,
+            807,
+            808,
+            760,
+            761,
+            714,
+            715,
+            667,
+            620,
+            621,
+            622,
+            574,
+            527,
+            479
+        };
     }
-
-    private int[] cellIndices = {
-        192,
-        144,
-        97,
-        49,
-        50,
-        51,
-        100,
-        148,
-        149,
-        150,
-        151,
-        104,
-        56,
-        57,
-        106,
-        154,
-        203,
-        250,
-        249,
-        297,
-        345,
-        346,
-        395,
-        442,
-        441,
-        489,
-        537,
-        586,
-        634,
-        635,
-        588,
-        540,
-        541,
-        590,
-        638,
-        639,
-        592,
-        543,
-        496,
-        497,
-        498,
-        546,
-        595,
-        596,
-        597,
-        598,
-        646,
-        695,
-        743,
-        744,
-        745,
-        746,
-        747,
-        748,
-        749,
-        702,
-        654,
-        607,
-        608,
-        656,
-        704,
-        752,
-        753,
-        706,
-        707,
-        708,
-        709,
-        757,
-        758,
-        807,
-        808,
-        760,
-        761,
-        714,
-        715,
-        667,
-        620,
-        621,
-        622,
-        574,
-        527,
-        479    
-    };
 }
+
 
 
--- Assets/Scripts/Water/RiverCell.cs
+++ Assets/Scripts/Water/RiverCell.cs
@@ -6,15 +6,93 @@ namespace Water {
     public class RiverCell : MonoBehaviour {
 
         public int index;
-        public Water Water;
+        public RiverThames river;
+        public HexCell hexCell;
 
+        public RiverCell PreviousCell { get; set; }
+        public RiverCell NextCell { get; set; }
 
-        private void Awake() {
-            
+        private Water[] flow = new Water[2];
+
+        private Water inflow;
+
+        public Water Flow {
+            get {
+                if (flow[0].Volume != 0) {
+                    return flow[0];
+                }
+                else if (flow[1].Volume != 0) {
+                    return flow[1];
+                }
+                else {
+                    return new Water(0, 0);
+                }
+            }
         }
 
+        private void Awake() {
 
+            flow[0] = new Water(5200000000, 1);
+            flow[1] = new Water(0, 1);
+
+        }
+
+        public void UpdateFlow(bool toggle) {
+
+            Discharge(river.GetInflow(this));
+
+            if (toggle == true) {
+                if (index > 0) {
+                    flow[1] = PreviousCell.flow[0];
+                    PreviousCell.flow[0] = new Water(0, 1);
+                }
+                flow[1].Volume += inflow.Volume;
+                if (flow[1].Volume + inflow.Volume > 0) {
+                    flow[1].Quality = (flow[1].Product + inflow.Product) / (flow[1].Volume + inflow.Volume);
+                }
+            }
+            else if (toggle == false) {
+                if (index > 0) {
+                    flow[0] = PreviousCell.flow[1];
+                    PreviousCell.flow[1] = new Water(0, 1);
+                }
+                flow[0].Volume += inflow.Volume;
+                if (flow[0].Volume + inflow.Volume > 0) {
+                    flow[0].Quality = (flow[0].Product + inflow.Product) / (flow[0].Volume + inflow.Volume);
+                }
+            }
+            else {
+                throw new System.ArgumentNullException("The toggle must be either true or false;");
+            }            
+
+            inflow.Volume = 0;
+
+            if (flow[0].Volume == 0 && flow[1].Volume == 0) {
+                hexCell.MainColor = Color.white;
+            }
+        }
+
+        public void Discharge(Water discharge) {           
+
+
+            if (inflow.Volume + discharge.Volume > 0) {
+                inflow.Quality = (inflow.Product + discharge.Product) / (inflow.Volume + discharge.Volume);
+            }
+            else {
+                inflow.Quality = 1;
+            }
+
+            inflow.Volume += discharge.Volume;
+        }
+
+        //public Water Abstract(float demand) {
+
+        //    if (demand <= Water.Volume) {
+        //        return new Water(demand, Water.Quality);
+        //    }
+        //    else {
+        //        return new Water(Water.Volume, Water.Quality);
+        //    }
+        //}
     }
 }
-
-
