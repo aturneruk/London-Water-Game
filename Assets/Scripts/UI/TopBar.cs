433208bc6ecea3cf2a08b45a3aeb49f748b59aec
Adam Turner <2829581+aturneruk@users.noreply.github.com>
2021-04-27 18:08:00 +0100

    Implement play/pause button

--- Assets/Scripts/UI/TopBar.cs
+++ Assets/Scripts/UI/TopBar.cs
@@ -6,26 +6,40 @@ using UnityEngine.UI;
 namespace UI {
     public class TopBar : MonoBehaviour {
 
-        public Text date;
-        public Text population;
-
-        public Button speedControl;
-
         public HexGrid grid;
 
+        public bool paused;
+
+        [SerializeField]
+        private Button speedControl, playPauseButton;
+
+        [SerializeField]
+        private Text date, population;
+
+        [SerializeField]
+        private CanvasGroup playImage, pauseImage;
+
         void Start() {
+            paused = true;
+            SwapToImage(playImage);
+
             GameTime.Set(1800, Month.Jan, 1, Weekday.Mon);
             GameTime.SetSpeed(Speed.X1);
             speedControl.GetComponentInChildren<Text>().text = Speed.X1.ToString();
         }
 
         void Update() {
-            GameTime.UpdateTime();
+
+            if (!paused) {
+                GameTime.UpdateTime();
+            }
+
             date.text = GameTime.GetLongForm();
             population.text = "Population: " + grid.GetTotalPopulation().ToString();
+            
         }
 
-        public void SpeedChange() {
+        public void ChangeSpeed() {
 
             Speed speed;
 
@@ -39,6 +53,41 @@ namespace UI {
             }
 
             speedControl.GetComponentInChildren<Text>().text = speed.ToString();
+        }
+
+        public void TogglePlayPause() {
+
+            paused = !paused;
+
+            if (paused) {
+                SwapToImage(playImage);
+            }
+            else if (!paused) {
+                SwapToImage(pauseImage);
+            }
+            else {
+                throw new System.ArgumentNullException("The play/pause boolean must be defined");
+            }
+        }
+
+        public void SwapToImage(CanvasGroup image) {
+            image.alpha = 1f;
+            image.interactable = true;
+            image.blocksRaycasts = true;
+
+            if (image == playImage) {
+                pauseImage.alpha = 0f;
+                pauseImage.interactable = false;
+                pauseImage.blocksRaycasts = false;
+            }
+            else if (image == pauseImage) {
+                playImage.alpha = 0f;
+                playImage.interactable = false;
+                playImage.blocksRaycasts = false;
+            }
+            else {
+                throw new System.ArgumentOutOfRangeException("The image CanvasGroup must either be the play or pause image");
+            }            
         }
     }
 }
